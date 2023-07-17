01f50694d7d63e52d84dd09fd30e85d147b06917
Ley19 <santillanleylani19@gmail.com>
2023-07-16 19:16:34 -0600

    Scanner y pantallas

--- app/src/main/java/com/example/navbotdialog/Orden/MostrarOrden.java
+++ app/src/main/java/com/example/navbotdialog/Orden/MostrarOrden.java
@@ -1,2 +1,31 @@
-package com.example.navbotdialog.Orden;public class MostrarOrden {
+package com.example.navbotdialog.Orden;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.navbotdialog.R;
+
+public class MostrarOrden extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragmento_mostrar_orden, container, false);
+
+
+
+        return rootView;
+
+    }
 }
--- app/src/main/java/com/example/navbotdialog/Scanner/ScannerActivity.java
+++ app/src/main/java/com/example/navbotdialog/Scanner/ScannerActivity.java
@@ -1,16 +1,47 @@
 package com.example.navbotdialog.Scanner;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ScannerActivity extends AppCompatActivity {
+import com.example.navbotdialog.R;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class ScannerActivity extends Activity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_scanner);
+        // Inicia el escáner
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.initiateScan();
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Procesa el resultado del escaneo
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() != null) {
+                // Aquí obtienes el contenido del código escaneado
+                String scannedData = result.getContents();
+                // Puedes hacer lo que necesites con el contenido escaneado
+                Toast.makeText(this, scannedData, Toast.LENGTH_SHORT).show();
+
+            } else {
+                // Si el escaneo no fue exitoso
+            }
+        }
     }
 
 }
